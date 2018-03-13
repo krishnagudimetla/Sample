53f19f8a080f918ddcca7d1ff045ae87f04e095f
krishnagudimetla <33870390+krishnagudimetla@users.noreply.github.com>
2018-03-13 15:18:32 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        
+        int number = 10;
+
+        // println() prints the following line to the output screen
+        System.out.println("You entered: " + number);
+    }
+}
